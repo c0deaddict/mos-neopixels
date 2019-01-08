e96f711e295ce7870e4be9bc4b5875b32e08e636
Jos van Bakel <josvanbakel@protonmail.com>
2019-01-08 22:14:12 +0100

    initial version

---
+++ build/gen/build_info.c
@@ -0,0 +1,4 @@
+/* Auto-generated, do not edit. */
+const char *build_id = "20190107-203833";
+const char *build_timestamp = "2019-01-07T20:38:33Z";
+const char *build_version = "1.0";
---
+++ build/gen/mg_build_info.c
@@ -0,0 +1,4 @@
+/* Auto-generated, do not edit. */
+const char *mg_build_id = "20190107-203832";
+const char *mg_build_timestamp = "2019-01-07T20:38:32Z";
+const char *mg_build_version = "201901072038";
